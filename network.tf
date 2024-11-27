dfc881667a8d2663a8ee5b6a0bd384a6f29dbe88
vigneshs-dev <vigneshkavery97@gmail.com>
2024-11-27 12:05:15 +0000

    Create Networking base

--- network.tf
+++ network.tf
@@ -7,3 +7,29 @@ locals {
   name_prefix = lower("${var.app_name}-${var.environment}")
   azs         = slice(data.aws_availability_zones.available.names, 0, var.az_count)
 }
+
+module "network" {
+  source               = "terraform-aws-modules/vpc/aws"
+  version              = "4.0.1"
+  name                 = local.name_prefix
+  cidr                 = var.vpc_cidr
+  enable_dns_hostnames = var.enable_dns_hostnames
+
+  azs = local.azs
+
+  # Public subnets
+  public_subnets = [for key, value in local.azs : cidrsubnet(var.vpc_cidr, var.public_newbits, key + var.public_newnum)]
+  public_subnet_tags = {
+    "Tier" = "Web"
+  }
+  map_public_ip_on_launch = var.map_public_ip_on_launch
+
+  # Private subnets
+  private_subnets = [for key, value in local.azs : cidrsubnet(var.vpc_cidr, var.private_newbits, key + var.private_newnum)]
+  private_subnet_tags = {
+    "Tier" = "Database"
+  }
+
+  # NAT
+  enable_nat_gateway     = var.enable_nat_gateway
+}
